873a0450d823ced3b897ab42497bcaabb554a5b0
Arcelyth <arcelyth@tutamail.com>
2026-03-30 00:05:49 +0800

    add sort_path and test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::ffi::c_void;
 use std::fs::{self, File};
-use std::io::{Read};
+use std::io::Read;
 use std::ptr;
 
 use live_ascii::context::*;
@@ -47,15 +47,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     // load texture
+    let mut png_files = vec![];
     let mut textures = vec![];
     for entry in fs::read_dir(args.texture)? {
         let entry = entry?;
         let path = entry.path();
-        
+
         if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("png") {
-            let texture = image::open(path)?.to_rgba8();
-            textures.push(image::DynamicImage::ImageRgba8(texture));
+            png_files.push(path);
         }
+    }
+
+    sort_path(&mut png_files);
+
+    for file in png_files {
+        let texture = image::open(file)?.to_rgba8();
+        textures.push(image::DynamicImage::ImageRgba8(texture));
     }
 
     let mut renderer = Renderer::new(model_ptr, textures);
@@ -64,8 +71,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut context = Context::new(false);
 
     renderer.render(&mut context)?;
-    
+
     Ok(())
 }
-
-
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use std::path::PathBuf;
 use std::ptr;
 
 pub fn allocate_aligned(size: usize, alignment: usize) -> *mut u8 {
@@ -6,4 +7,47 @@ pub fn allocate_aligned(size: usize, alignment: usize) -> *mut u8 {
         libc::posix_memalign(&mut ptr, alignment, size);
     }
     ptr as *mut u8
-} 
+}
+
+pub fn sort_path(files: &mut Vec<PathBuf>) {
+    files.sort_by_key(|path| path.file_name().unwrap().to_string_lossy().to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn sort_path_test() {
+        let mut files = vec![
+            PathBuf::from("texture_09.png"),
+            PathBuf::from("texture_03.png"),
+            PathBuf::from("texture_06.png"),
+            PathBuf::from("texture_08.png"),
+            PathBuf::from("texture_01.png"),
+            PathBuf::from("texture_07.png"),
+            PathBuf::from("texture_11.png"),
+            PathBuf::from("texture_02.png"),
+            PathBuf::from("texture_10.png"),
+            PathBuf::from("texture_00.png"),
+            PathBuf::from("texture_04.png"),
+            PathBuf::from("texture_05.png"),
+        ];
+        sort_path(&mut files);
+        let expected = vec![
+            PathBuf::from("texture_00.png"),
+            PathBuf::from("texture_01.png"),
+            PathBuf::from("texture_02.png"),
+            PathBuf::from("texture_03.png"),
+            PathBuf::from("texture_04.png"),
+            PathBuf::from("texture_05.png"),
+            PathBuf::from("texture_06.png"),
+            PathBuf::from("texture_07.png"),
+            PathBuf::from("texture_08.png"),
+            PathBuf::from("texture_09.png"),
+            PathBuf::from("texture_10.png"),
+            PathBuf::from("texture_11.png"),
+        ];
+
+        assert_eq!(files, expected);
+    }
+}
